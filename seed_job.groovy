158f18c66d8b5a75235fc10eeee8551810487ce9
alexthai <axt.421@gmail.com>
2016-08-28 12:55:25 -0400

    Updated seed_job.groovy

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,3 +1,15 @@
 freeStyleJob('seed_job'){
+	scm {
+        git {
+            remote {
+                name('unity-jenkins')
+                url('https://github.com/alexthaii/unity-jenkins.git')
+            }
+    }
 
+    triggers {
+        githubPush()
+    }
+
+    dslFile('seed_job.groovy')
 }
